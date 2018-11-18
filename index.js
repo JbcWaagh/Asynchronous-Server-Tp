652e91a0f1d55f1e2d127f6c4a2fc11faced0c23
JbcWaagh <32640152+JbcWaagh@users.noreply.github.com>
2018-11-18 22:44:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const url = require('url')
-const qs = require('querystring')
-const http = require('http')
+express = require('express')
+app = express()
+
+app.set('port', 8080)
+
+app.listen(
+  app.get('port'), () => console.log(`server listening on ${app.get('port')}`)
+)
 
 
-const handles = require('./handles')
-const server = http.createServer(handles.serverHandle);
-server.listen(8080)
+app.get(
+  '/',
+  (req, res) => res.send('Ajouter /hello/[Votre nom]  pour que la page vous salue')
+)
+
+app.get(
+  '/hello/:name',
+  (req, res) => {
+    if (req.params.name == 'jean-baptiste'){
+
+      res.send('jean-baptiste Crespo createur de cette page, age: 22 ans, mots favoris: Amphigouri ')
+
+    } else {
+
+      res.send("Hello " + req.params.name)
+
+    }
+  }
+)
+
+
+
+app.use(function(req, res){
+       res.send('ERROR 404: page not found');
+   });
